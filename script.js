59fb3451066446d6dcc0660ab6c32f74e3e0f4f9
Devika <devikab2021@gmail.com>
2023-01-12 19:57:07 +0530

    Working extension

--- script.js
+++ script.js
@@ -1,70 +1,29 @@
-// alert("Hello")
-
 const btn = document.getElementById("btn")
+btn.addEventListener("click", () => {
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+        var search = (tabs[0].url);
+        getRepos(search)
+    });
+})
 
-btn.addEventListener("click", getRepos)
-
-
-// var form = document.getElementById("myForm")
-// form.addEventListener("submit", function(e) {
-//     e.preventDefault()
-
-
-
-    // var search = document.getElementById("search").value 
-    // var origName = search.split('https://').join('')
-    // var originalName = origName.split('.com').join('')
-   
-   
-   
-    // var repo = document.getElementById("rsearch").value
-    // var repo1 = repo.split('https://').join('')
-    // var repo2 = repo1.split('.com').join('')
-    // getRepos()
-
-
-
-    // var page = 1
-    // var per_page = 1
-    // var sort = "created"
-    // var order = 
-    // fetch("https://api.github.com/repos/" + originalName + "/" + repo)
+async function getRepos(search) {
+    var repo1 = search.split('https://').join('')
+    var repo = repo1.split(".com").join("")
     
+    const url = "https://api.github.com/search/repositories?q="+repo+";per_page=1"
+    console.log(url)
+    btn.remove()
+    const divResult = document.getElementById("divResult")
+    const response = await fetch(url)
+    const result = await response.json()
+    result.items.forEach(i=>{
 
-
-    // fetch("https://api.github.com/search/repositoris?q="+repo2)
-    // .then((result) => result.json())
-    // .then((data) => {
-    //     console.log(data)
-
-
-
-        // document.getElementById('result').innerHTML = `
-        // <h1> ${data.created_at} </h1>
-        // <a href="${data.clone_url.split('.git').join('')}">${data.clone_url.split('.git').join('')}</a>
-        // `
-
-
-
-    // 
-    
-    
-
-    async function getRepos(){
-        const repo = document.getElementById("rsearch").value
-        const url = "https://api.github.com/search/repositories?q="+repo+";per_page=1"
-        console.log(url)
-        const divResult = document.getElementById("divResult")
-        const response = await fetch(url)
-        const result = await response.json()
-        result.items.forEach(i=>{
-
-            const anchor = document.createElement("a")
-            anchor.href = i.html_url
-            anchor.textContent = i.full_name
-            divResult.appendChild(anchor)
-            divResult.appendChild(document.createElement("br"))
-        })
-    }
-    
-// })
+        const anchor = document.createElement("a")
+        anchor.href = i.html_url
+        console.log(anchor.href)
+        anchor.target = "_blank"
+        anchor.textContent = i.full_name
+        divResult.appendChild(anchor)
+        divResult.appendChild(document.createElement("br"))
+    })
+};
